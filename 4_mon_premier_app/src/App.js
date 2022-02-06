17dd0527522fcc5af556cefd2b5a00124013781e
lapinRagnar <lapinragnar@gmail.com>
2022-02-06 20:28:51 +0100

    convertir composant en class en composant fonctionnel (utilisation des hooks)

--- 4_mon_premier_app/src/App.js
+++ 4_mon_premier_app/src/App.js
@@ -1,51 +1,67 @@
-import React from "react"
+import React, { useState } from "react"
 import Picture from "./components/Picture";
 import "./css/app.css"
 
 // composant a base de fonction
 
-// function App({title}){
-//     return <h1 className="heading" >Bonjour les amis - {title} </h1>
-// }
+function App(){
+
+    // pour declarer les varibles d'état on va utiliser les hooks
+    const [titre, setTitre] = useState("Bonjour les amis!")
+    const [show, setShow] = useState(false)
+
+    function handleClick () {
+        setShow(!show)
+    }
+
+    return (
+        <div>
+            <h1>{titre} </h1>
+            <p> cliquer pour afficher l'image</p>
+            { show ? <Picture/> : null }
+            <button className="element-btn" onClick={handleClick}>Cliquez</button>
+        </div>
+    )
+}
 
 // composant a base de classe 
 
-class App extends React.Component{
+// class App extends React.Component{
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            titre: '1ere evenement avec React js pour interagir avec le dom',
-            show: false
-        }
-        console.log("contructeur appelé!");
-    }
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             titre: '1ere evenement avec React js pour interagir avec le dom',
+//             show: false
+//         }
+//         console.log("contructeur appelé!");
+//     }
 
-    componentDidMount(){
-        console.log("composant monté!");
-        this.setState({titre: "le composant a été monté"})    
-    }
+//     componentDidMount(){
+//         console.log("composant monté!");
+//         this.setState({titre: "le composant a été monté"})    
+//     }
 
-    componentDidUpdate() {
-        console.log("le composant mise a jour!");
-    }
+//     componentDidUpdate() {
+//         console.log("le composant mise a jour!");
+//     }
 
-    render() {
+//     render() {
 
-        console.log("render appelé!");
+//         console.log("render appelé!");
 
-        return <div>
-            <h1>{this.state.titre} </h1>
-            <p> cliquer pour afficher l'image</p>
-            {
-                this.state.show ?
-                <Picture/>
-                : null
-            }
-            <button className="element-btn" onClick={()=>{this.setState({show: !this.state.show})}}>Cliquez</button>
-        </div>
-    }
-}
+//         return <div>
+//             <h1>{this.state.titre} </h1>
+//             <p> cliquer pour afficher l'image</p>
+//             {
+//                 this.state.show ?
+//                 <Picture/>
+//                 : null
+//             }
+//             <button className="element-btn" onClick={()=>{this.setState({show: !this.state.show})}}>Cliquez</button>
+//         </div>
+//     }
+// }
 
 export default App
 
