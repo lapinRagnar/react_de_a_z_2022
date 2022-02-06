1574173a4564c92542cfba7a59e012dd0ea9235e
lapinRagnar <lapinragnar@gmail.com>
2022-02-06 10:39:31 +0100

    composant à base de class

--- 4_mon_premier_app/src/App.js
+++ 4_mon_premier_app/src/App.js
@@ -1,7 +1,19 @@
+import React from "react"
 import "./css/app.css"
 
-function App({title}){
-    return <h1 className="heading" >Bonjour les amis - {title} </h1>
+// composant a base de fonction
+
+// function App({title}){
+//     return <h1 className="heading" >Bonjour les amis - {title} </h1>
+// }
+
+// composant a base de classe 
+
+class App extends React.Component{
+
+    render() {
+        return <h1 className="heading" >Bonjour les amis </h1>
+    }
 }
 
 export default App
