9b8d503d2dd802a2e1b3ab4d901c374e5fbb7e3c
lapinRagnar <lapinragnar@gmail.com>
2022-02-06 10:07:43 +0100

    ajouter des styles css inline dans le composant

--- 4_mon_premier_app/src/App.js
+++ 4_mon_premier_app/src/App.js
@@ -1,6 +1,6 @@
 
 function App({title}){
-    return <h1>Bonjour les amis - {title} </h1>
+    return <h1 style={{fontSize: '4rem', color: 'greenyellow', textAlign:'center', textDecoration:'underline red'}} >Bonjour les amis - {title} </h1>
 }
 
 export default App
