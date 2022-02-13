485d8111d28f1a512be7c7b54ccdea5c7f5b68dd
lapinRagnar <lapinragnar@gmail.com>
2022-02-13 10:07:41 +0100

    afficher plusieurs images avec la methode map

--- 4_mon_premier_app/src/components/Picture.js
+++ 4_mon_premier_app/src/components/Picture.js
@@ -4,6 +4,15 @@ import React, { Component, useEffect, useState } from 'react';
 
 export default function Picture() {
 
+    const images = [
+        "nord1.jpg",
+        "nord2.jpg",
+        "nord3.jpg",
+        'mon-profile.jpg'
+    ]
+
+    
+
 //   const [myTimer, setMyTimer] = useState(null)
 
   // quand le composant est monté
@@ -20,7 +29,13 @@ export default function Picture() {
 
   return (
       <div>
-          <img className="mon-profile" src="mon-profile.jpg" alt="" />
+          {
+              images.map((name)=>{
+                return(
+                    <img className="mon-profile" src={name} alt="" />
+                )
+              })
+          }
       </div>
   )
 }
