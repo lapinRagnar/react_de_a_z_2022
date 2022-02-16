b3a8d9a2b895324d4e9945437dd150a2ea3d0560
lapinRagnar <lapinragnar@gmail.com>
2022-02-16 19:17:46 +0100

    Créer Header component et styliser avec styled-components

--- 8_disney_plus_clone/src/components/Header.js
+++ 8_disney_plus_clone/src/components/Header.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Header() {
+
   return (
-    <div>Header</div>
+    
+    <Container>
+        Header
+    </Container>
   )
-}
+
+}   
 
 export default Header
+
+const Container = styled.div`
+    background: #090b13;
+    height: 70px;
+
+`
+
