11568cc1375e84a296e0026af670f282087ec6e2
lapinRagnar <lapinragnar@gmail.com>
2022-02-24 20:57:23 +0100

    styliser css dans un component

--- 10_firebase/src/App.js
+++ 10_firebase/src/App.js
@@ -1,14 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Dot = ({color}) => {
+
+  const style = {
+    height: 50,
+    width: 50,
+    margin: "0px 10px",
+    backgroundColor: color,
+    borderRadius: "50%",
+    display: "inline-block"
+  }
   return (
-    <div className="App">
+    <span style={style}></span>
+  )
+}
+
+function App() {
+
+
+  return (
+    <div>
         <h1>Bonjour!</h1>
         <button>Nouveau</button>
         <ul>
           <li>
-            <a href="#">editer</a>
+            <a href="#">editer</a> <Dot color="#f00" /> Red
+          </li>
+          <li>
+            <a href="#">editer</a> <Dot color="#0f0" /> Green
+          </li>
+          <li>
+            <a href="#">editer</a> <Dot color="#00f" /> Blue
           </li>
         </ul>
     </div>
