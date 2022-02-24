c36d723e4edefe9a7f2f5f9fa1e6adaaa2d17e04
lapinRagnar <lapinragnar@gmail.com>
2022-02-24 20:44:23 +0100

    new tuto - ajouter firebase firestore - color palette

---
+++ 10_firebase/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Bonjour!</h1>
+        <button>Nouveau</button>
+        <ul>
+          <li>
+            <a href="#">editer</a>
+          </li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
