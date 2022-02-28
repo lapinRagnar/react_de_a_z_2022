981d64e16595c8ab4f276a5a477cc7418695076b
lapinRagnar <lapinragnar@gmail.com>
2022-02-28 18:01:20 +0100

    creer email et mot de passe - connexion au firebase auth - .env

--- 11_authentication/src/App.js
+++ 11_authentication/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { signup } from './firebase'
+import { useRef } from 'react';
 
 function App() {
+
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  async function handleSignup() {
+    await signup(emailRef.current.value, passwordRef.current.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Authentication tuto</h1>
+
+      <div id='fields'>
+        <input ref={emailRef} type="text" placeholder='email' />
+        <input ref={passwordRef} type="password" name="" id="" placeholder='mot de passe!' />
+      </div>
+
+      <button onClick={handleSignup}>Sign up</button>
+
     </div>
   );
 }
