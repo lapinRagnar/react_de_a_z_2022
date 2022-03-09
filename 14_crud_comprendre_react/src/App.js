717a718b33d9d3c4cfefc2c6b046b7632d83ae71
lapinRagnar <lapinragnar@gmail.com>
2022-03-09 18:26:25 +0100

    afficher ou interpoler des variables dans le render

--- 14_crud_comprendre_react/src/App.js
+++ 14_crud_comprendre_react/src/App.js
@@ -3,18 +3,33 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+
+  state = {
+    clients: [
+      {id: 1, nom: 'client 1'},
+      {id: 2, nom: 'client 2'},
+      {id: 3, nom: 'client 3' }
+    ]
+  }
+
   render() {
+    const title = 'liste des clients'
+    const element = <li>test de varibles</li>
+    const elements = [
+      <li>premier element</li>,
+      <li>deuxieme element</li>
+    ]
+
     return <div>
-      <h1>Liste de client!</h1>
+      <h1>{title} </h1>
       <ul>
-        <li>client 1 <button>X</button> </li> 
-        <li>client 2 <button>X</button> </li> 
-        <li>client 3 <button>X</button> </li> 
+        {element}
+        {elements}
       </ul>
       <form action="">
         <input type="text" placeholder='ajouter un client' />
         <button>Confirmer</button>
-        
+
       </form>
     </div>
   }
