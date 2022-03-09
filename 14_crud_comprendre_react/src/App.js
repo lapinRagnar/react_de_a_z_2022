dfad309a212d1c087e8ce9e69ac2d1288b8aeb96
lapinRagnar <lapinragnar@gmail.com>
2022-03-09 18:16:26 +0100

    crud - read - affcher client en dur d'abord

--- 14_crud_comprendre_react/src/App.js
+++ 14_crud_comprendre_react/src/App.js
@@ -4,7 +4,19 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return <h1>Bonjour à tous!</h1>
+    return <div>
+      <h1>Liste de client!</h1>
+      <ul>
+        <li>client 1 <button>X</button> </li> 
+        <li>client 2 <button>X</button> </li> 
+        <li>client 3 <button>X</button> </li> 
+      </ul>
+      <form action="">
+        <input type="text" placeholder='ajouter un client' />
+        <button>Confirmer</button>
+        
+      </form>
+    </div>
   }
 }
 
