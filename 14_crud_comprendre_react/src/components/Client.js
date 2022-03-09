b6408eb7729ac0216dbb90525566ff78600e1549
lapinRagnar <lapinragnar@gmail.com>
2022-03-09 21:43:15 +0100

    refactoring

--- 14_crud_comprendre_react/src/components/Client.js
+++ 14_crud_comprendre_react/src/components/Client.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
-const Client = (props) => {
+const Client = ({ details, onDelete }) => {
 
     // const details = this.props.details
     // const onDelete = this.props.onDelete
     // sont l'equivalent de 
 
-    const { details, onDelete } = props
+    // const { details, onDelete } = props
 
 
     return (
         <li key={details.id} >
-            {details.nom} <button onClick={() => onDelete(props.details.id)}>X</button>             
+            {details.nom} <button onClick={() => onDelete(details.id)}>X</button>             
         </li>
     );
     
