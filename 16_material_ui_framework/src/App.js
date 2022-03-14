b140f7b7de7f0a9596e91fa51b6197f6b0613ebf
lapinRagnar <lapinragnar@gmail.com>
2022-03-14 13:29:27 +0100

    overrider le theme par defaut - des composants

--- 16_material_ui_framework/src/App.js
+++ 16_material_ui_framework/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
 
 function App() {
   return (
     <div>
       <Button disabled>bouton disable</Button>
+      
       <Button fullWidth>fullWidth</Button>
+      
       <Button 
         fullWidth
         variant='outlined'
       >fullWidth
       </Button>
+      
       <Button 
         fullWidth
         color='primary'
       >
         Bouton avec couleur
       </Button>
+
+      <Button 
+        fullWidth
+        color='secondary'
+      >
+        Bouton avec couleur secondary
+      </Button>
+
+
     </div>
   );
 }
