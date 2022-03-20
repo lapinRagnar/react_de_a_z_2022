c48596595b4790f41bd3f9e9fd72dac336fe02c2
lapinRagnar <lapinragnar@gmail.com>
2022-03-20 12:13:23 +0100

    enfant vers parent avec destrucuration

--- 17_passer_des_donnees_entre_composant/src/pages/Enfant1.js
+++ 17_passer_des_donnees_entre_composant/src/pages/Enfant1.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
-export default function Enfant1(props) {
+export default function Enfant1({valeurParent, setValeurParent}) {
 
     console.log("je suis dans enfant 1");
-    console.log(props.valeurParent);
+    console.log(valeurParent);
   return (
       <>
          <div>Enfant1</div>
-         <div>La valeur du parent est =  {props.valeurParent} </div>
+         <div>La valeur du parent est =  {valeurParent} </div>
          <h1>Donner la valeur à modifier pour modifier la valeur du parent</h1>
          <input 
             type="number"
             placeholder='tapez un nombre'
-            onChange={e => props.setValeurParent(e.target.value)}
+            onChange={e => setValeurParent(e.target.value)}
             />
       </>
   )
